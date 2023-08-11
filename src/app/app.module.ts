1787e3367cc5672dadab400812ecadf93c4109b1
Gerard Sans <gerard.sans@gmail.com>
2023-08-11 16:02:00 +0200

    Add predict component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PredictComponent } from './predict/predict.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PredictComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
